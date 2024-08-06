b3b8bd711b6879794621f37f7ec15560b5d8c37e
Thanasuk00 <thanasuk4745@gmail.com>
2024-08-06 11:53:37 +0700

    new update django

---
+++ webpage/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("",  views.indexPage, name="index"),
+    path("about/", views.aboutUs, name="about"),
+    path("contact/", views.contactUs),
+]
---
+++ webpage/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def indexPage(request):
+    return render(request, 'index.html')
+
+def aboutUs(request):
+    return render(request, 'about.html')
+
+
+def contactUs(request):
+    return render(request, 'contact.html')
