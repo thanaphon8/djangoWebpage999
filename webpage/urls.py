14261c74450759915fd95f700a6228f51f692510
thanaphon8 <dthanaphon21@gmail.com>
2024-08-07 14:58:51 +0700

    commit1

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path("",  views.indexPage, name="index"),
+    path("", views.indexPage, name="index"),
     path("about/", views.aboutUs, name="about"),
-    path("contact/", views.contactUs),
+    path("contact/", views.contactUs, name="contact"),
+    path("for/", views.forPage, name="for-page"),
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+
+
 def indexPage(request):
     return render(request, 'index.html')
+
 
 def aboutUs(request):
     return render(request, 'about.html')
@@ -10,3 +13,7 @@ def aboutUs(request):
 
 def contactUs(request):
     return render(request, 'contact.html')
+
+
+def forPage(request):
+    return render(request, 'for_test.html')
